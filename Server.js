1444e2698acb1e8301c9fac4897db19c3498c400
vatsalvataliya <138905407+vatsalvataliya@users.noreply.github.com>
2024-04-10 20:57:06 +0530

    update 4

--- Server.js
+++ Server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express, { json } from 'express';
+import { connect } from 'mongoose';
  
 require('dotenv').config();
 
-const routes = require("./routes/TaskRoute");
+import routes from "./routes/TaskRoute";
 
-const cors = require('cors');
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected...')).catch((err) => console.log(err));
+connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected...')).catch((err) => console.log(err));
 
 // app.get('/',(req,res) => {
 //     res.send('The Brave Coders');
